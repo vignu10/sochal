79a0c89fc3403cb43ecb638ff87112bca1befe3b
vignu <vignuday2501@gmail.com>
2024-06-15 20:43:25 +0530

    consolidated front-end and backend to setup docker

---
+++ frontend/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+     <div>
+        <div className="p-5 border border-emerald-900  "></div>
+     </div>
+  );
+}
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -7,7 +7,26 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      'max-sm': {max: '640px'},
+      'sm': '640px',
+      'md': '768px',
+      'max-md': {max: '920px'},
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1439px',
+       '3xl': '2560px'
+    },
     extend: {
+      borderWidth: {
+        DEFAULT: '1px',
+        '0': '0',
+        '2': '2px',
+        '3': '3px',
+        '4': '4px',
+        '6': '6px',
+        '8': '8px',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
